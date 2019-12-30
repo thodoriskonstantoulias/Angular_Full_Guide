512341fc612dbadf7057ebc5a8cd48ca5179dee2
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-30 12:52:54 +0200

    Inline elements, style elements

---
+++ Angular-Guide/src/app/servers/servers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  template: '<h4>Inside servers comp with inline html</h4>',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
