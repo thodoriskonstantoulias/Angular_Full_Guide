bdb2caad7634bf84ff8f881c5f0e77f059928750
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-30 20:55:04 +0200

    #Set the basics for Recipe list

--- Angular-Project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular-Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes : Recipe[] = [
+    new Recipe('Test Recipe','Test description',
+    'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-09-how-to-shrimp-alfredo%2FHT-Shrimp-Alfredo_103')
+  ];
 
   constructor() { }
 
