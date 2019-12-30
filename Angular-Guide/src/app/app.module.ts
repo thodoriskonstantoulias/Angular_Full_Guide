66ff62f58c95de64f0f7bc2736f921e033c98cbf
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-30 17:30:50 +0200

    Clearing up test project

--- Angular-Guide/src/app/app.module.ts
+++ Angular-Guide/src/app/app.module.ts
@@ -4,14 +4,12 @@ import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 import { Exercise1Component } from './exercise1/exercise1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent,
     Exercise1Component
   ],
   imports: [
