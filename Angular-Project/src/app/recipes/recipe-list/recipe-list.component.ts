45fdd176a9f988ae969069f510049af95514ee3d
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-31 21:45:28 +0200

    #Passing data from recipe item to recipe detail when clicked

--- Angular-Project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular-Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,14 +8,21 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes : Recipe[] = [
     new Recipe('Test Recipe','Test description',
+    'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-09-how-to-shrimp-alfredo%2FHT-Shrimp-Alfredo_103'),
+    new Recipe('Test Recipe 2','Test description',
     'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-09-how-to-shrimp-alfredo%2FHT-Shrimp-Alfredo_103')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe :Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
