9e036fa6c778b9b967f0b0da67bbad70307572f2
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-31 19:36:26 +0200

    Lifecycle hooks

--- Angular-Guide/src/app/server-element/server-element.component.ts
+++ Angular-Guide/src/app/server-element/server-element.component.ts
@@ -1,17 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit,AfterContentChecked,OnDestroy {
 
   @Input() element : {type :string, name:string, content:string};
 
-  constructor() { }
+  constructor() {
+    console.log('constructor called');
+   }
 
   ngOnInit() {
+    console.log('ngOnInit called');
   }
-
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
+  ngDoCheck() {
+    console.log('ngDoCheck called');
+  }
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called');
+  }
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called');
+  }
+  ngOnDestroy() {
+    console.log('ngOnDestroy called');
+  }
 }
