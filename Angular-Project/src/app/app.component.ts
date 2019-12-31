b21eb598257b29e8784de6f3531329f759257d29
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-31 20:35:33 +0200

    #Passing data from navigation bar to component

--- Angular-Project/src/app/app.component.ts
+++ Angular-Project/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Project';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature : string){
+    this.loadedFeature = feature;
+  }
 }
