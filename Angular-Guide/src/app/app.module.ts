6630a3042e3847e11863e2b43d538c7b022b71e4
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-31 13:18:49 +0200

    How to pass data from one component to the other - Input

--- Angular-Guide/src/app/app.module.ts
+++ Angular-Guide/src/app/app.module.ts
@@ -5,12 +5,18 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { Exercise1Component } from './exercise1/exercise1.component';
+import { Server2Component } from './server2/server2.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    Exercise1Component
+    Exercise1Component,
+    Server2Component,
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular-Guide/src/app/server2/server2.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server2',
+  templateUrl: './server2.component.html',
+  styleUrls: ['./server2.component.css']
+})
+export class Server2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  serverElements = [{type :'server', name:'Test server', content:'Test'}];
+  
+
+
+}
