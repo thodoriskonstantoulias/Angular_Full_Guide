ec08fa1802a38833d8858126184479882a0fad6d
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-01 18:24:43 +0200

    #Recipe service created with event listeners

--- Angular-Project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular-Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,21 +9,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes : Recipe[] = [
-    new Recipe('Test Recipe','Test description',
-    'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-09-how-to-shrimp-alfredo%2FHT-Shrimp-Alfredo_103'),
-    new Recipe('Test Recipe 2','Test description',
-    'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-09-how-to-shrimp-alfredo%2FHT-Shrimp-Alfredo_103')
-  ];
+  recipes : Recipe[];
 
-  constructor() { }
+  constructor(private recipeService : RecipeService) { }
 
   ngOnInit() {
-  }
-
-  onRecipeSelected(recipe :Recipe){
-    this.recipeWasSelected.emit(recipe);
+    this.recipes = this.recipeService.getRecipes();
   }
 
 }
--- Angular-Project/src/app/recipes/recipe.service.ts
+++ Angular-Project/src/app/recipes/recipe.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Injectable({
     providedIn : 'root'
 })
 
 export class RecipeService{
+    recipeSelected = new EventEmitter<Recipe>();
 
+    private recipes : Recipe[] = [
+        new Recipe('Test Recipe','Test description',
+        'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-09-how-to-shrimp-alfredo%2FHT-Shrimp-Alfredo_103'),
+        new Recipe('Test Recipe 2','Test description',
+        'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-09-how-to-shrimp-alfredo%2FHT-Shrimp-Alfredo_103')
+      ];
+
+    getRecipes(){
+        return this.recipes.slice();
+    }
 }
