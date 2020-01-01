93e8d8119c5d57297b04d0db837db4be920411fa
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-01 23:13:03 +0200

    #Add ingredients to shopping list through the details of the recipe

--- Angular-Project/src/app/recipes/recipe.service.ts
+++ Angular-Project/src/app/recipes/recipe.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable({
     providedIn : 'root'
@@ -10,12 +12,26 @@ export class RecipeService{
 
     private recipes : Recipe[] = [
         new Recipe('Test Recipe','Test description',
-        'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-09-how-to-shrimp-alfredo%2FHT-Shrimp-Alfredo_103'),
+        'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-09-how-to-shrimp-alfredo%2FHT-Shrimp-Alfredo_103',
+        [
+            new Ingredient('Meat',1),
+            new Ingredient('Potatoes',10)
+        ]),
         new Recipe('Test Recipe 2','Test description',
-        'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-09-how-to-shrimp-alfredo%2FHT-Shrimp-Alfredo_103')
+        'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-09-how-to-shrimp-alfredo%2FHT-Shrimp-Alfredo_103',
+        [
+            new Ingredient('Bread',2),
+            new Ingredient('Pork',1)
+        ])
       ];
+
+      constructor(private slService:ShoppingListService){}          
 
     getRecipes(){
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients:Ingredient[]){
+        this.slService.addIngredients(ingredients);
     }
 }
