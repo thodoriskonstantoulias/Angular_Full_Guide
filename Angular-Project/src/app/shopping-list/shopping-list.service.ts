fb9d611b68e16b75ebc1e245f57af24d46dcf0cb
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-01 22:38:59 +0200

    #Created Shopping list service

--- Angular-Project/src/app/shopping-list/shopping-list.service.ts
+++ Angular-Project/src/app/shopping-list/shopping-list.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
     providedIn : 'root'
@@ -6,4 +7,18 @@ import { Injectable } from '@angular/core';
 
 export class ShoppingListService{
 
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    private ingredients : Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10)
+      ];
+
+     getIngredients(){
+         return this.ingredients.slice();
+     } 
+
+     addIngredient(ingredient : Ingredient){
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+     }
 }
