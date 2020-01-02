a040f96c927ed14b72906a061d2c544da95ad089
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-02 23:06:33 +0200

    Pass and get query string though routing

--- Angular-Routing/src/app/home/home.component.ts
+++ Angular-Routing/src/app/home/home.component.ts
@@ -17,4 +17,8 @@ export class HomeComponent implements OnInit {
     this.router.navigate(['/servers']);
   }
 
+  onLoadServer(id : number){
+    this.router.navigate(['/servers',id,'edit'],{queryParams : {allowEdit:'1'}});
+  }
+
 }
--- Angular-Routing/src/app/servers/edit-server/edit-server.component.ts
+++ Angular-Routing/src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,9 +13,13 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,private route : ActivatedRoute) { }
 
   ngOnInit() {
+    //Retrieve params from route
+    //console.log(this.route.snapshot.params['id']);
+    //console.log(this.route.snapshot.queryParams);
+
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
