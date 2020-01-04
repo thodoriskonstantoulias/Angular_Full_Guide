7274401278338327e4d1f51153ac6163acb29d7c
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-04 20:22:07 +0200

    Examples of Observables

--- Angular-Routing/src/app/home/home.component.ts
+++ Angular-Routing/src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/Rx';
+import { Observer } from 'rxjs/Observer';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +15,38 @@ export class HomeComponent implements OnInit {
   constructor(private router : Router,private authService : AuthService) { }
 
   ngOnInit() {
+    //1st example of Observables
+    // const myNumbers = Observable.interval(1000);
+    // myNumbers.subscribe(
+    //   (number:number) => {
+    //     console.log(number);
+    //   }
+    // );
+
+    //2nd example of Observables
+    const myObservable = Observable.create((observer:Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000);
+      setTimeout(() => {
+        observer.next('second package');
+      }, 4000);
+      setTimeout(() => {
+        observer.error('error occured');
+      }, 5000);
+    });
+
+    myObservable.subscribe(
+      (data:string) => {
+        console.log(data);
+      },
+      (error:string) => {
+        console.log(error);
+      },
+      () => {
+        console.log('completed!');
+      }
+    );
   }
 
   onLoadServers(){
