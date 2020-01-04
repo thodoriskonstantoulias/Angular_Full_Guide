3b5feee92b62376d791de21eb1e13c5e14bc4044
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-04 21:47:05 +0200

    #Add subscription to our project

--- Angular-Project/src/app/shopping-list/shopping-list.service.ts
+++ Angular-Project/src/app/shopping-list/shopping-list.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
     providedIn : 'root'
@@ -7,7 +8,7 @@ import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService{
 
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
     private ingredients : Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10)
@@ -19,11 +20,11 @@ export class ShoppingListService{
 
      addIngredient(ingredient : Ingredient){
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
      }
 
      addIngredients(ingredients : Ingredient[]){
         this.ingredients.push(...ingredients);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
      }
 }
