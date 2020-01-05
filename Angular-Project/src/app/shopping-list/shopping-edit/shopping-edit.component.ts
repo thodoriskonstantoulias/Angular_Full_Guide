af3f49b2ecb72d136fbf047deceec27f4dd31cf1
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-05 20:56:18 +0200

    #Updating the ingredients correctly

--- Angular-Project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Angular-Project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -32,12 +32,18 @@ export class ShoppingEditComponent implements OnInit , OnDestroy{
         });
       }
     );
-  }
+  } 
 
-  onAddItem(form:NgForm){
+  onSubmit(form:NgForm){
     const value = form.value;
     const newIngredient = new Ingredient(value.name,value.amount);
-    this.slService.addIngredient(newIngredient);
+    if (this.editMode){
+      this.slService.updateIngredient(this.editedItemIndex,newIngredient);
+    } else {
+      this.slService.addIngredient(newIngredient);
+    }   
+    this.editMode = false;
+    form.reset();
   }
 
   ngOnDestroy(){
