a9e49dbedef57ce4d3a0cf147878a57a251e3079
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-05 12:53:54 +0200

    How are forms submitted

--- Angular-Forms/src/app/app.component.ts
+++ Angular-Forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,9 @@ export class AppComponent {
   title = 'Angular-Forms';
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: NgForm){
+    console.log(form);
   }
 }
