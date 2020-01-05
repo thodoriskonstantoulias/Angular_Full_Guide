ff79047af9e5b9d18a21ebe241b4cf0364f1f646
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-05 14:59:22 +0200

    Add radio button and displaying the form data

--- Angular-Forms/src/app/app.component.ts
+++ Angular-Forms/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -8,11 +8,26 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   title = 'Angular-Forms';
+  genders = ['male','female'];
+  @ViewChild('f',{static : true}) signUpForm : NgForm
+  user = {
+    username : '',
+    email : '',
+    secret : '',
+    gender : ''
+  }
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signUpForm.form.patchValue({username : suggestedName});
   }
 
   onSubmit(form: NgForm){
-    console.log(form);
+    this.user.username = form.value.username;
+    this.user.email = form.value.email;
+    this.user.secret = form.value.secret;
+    this.user.gender = form.value.gender;
+
+    form.reset();
   }
 }
