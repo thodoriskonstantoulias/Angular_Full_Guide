0a4d3cd75f0d120d9058746fadf903a839b01cab
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-06 00:26:58 +0200

    #Removing ingredients

--- Angular-Project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ Angular-Project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -84,4 +84,8 @@ export class RecipeEditComponent implements OnInit {
     this.router.navigate(['../'],{relativeTo : this.route})
   }
 
+  onDeleteIngredient(index:number){
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
+
 }
