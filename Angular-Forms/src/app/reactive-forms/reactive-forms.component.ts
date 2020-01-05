3a029e35307c1bc9e48fbb76982a565661e26ab4
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-05 17:36:44 +0200

    Custom validators

--- Angular-Forms/src/app/reactive-forms/reactive-forms.component.ts
+++ Angular-Forms/src/app/reactive-forms/reactive-forms.component.ts
@@ -10,12 +10,13 @@ export class ReactiveFormsComponent implements OnInit {
 
   genders = ['male', 'female'];
   signUpForm : FormGroup
+  forbiddenUsernames = ['Anna','Kostas'];
 
   constructor() { }
 
   ngOnInit() {
     this.signUpForm = new FormGroup({
-      'username' : new FormControl(null,Validators.required),
+      'username' : new FormControl(null,[Validators.required,this.forbiddenNames.bind(this)]),
       'email' : new FormControl(null,[Validators.required, Validators.email]),
       'gender' : new FormControl('male'),
       'hobbies' : new FormArray([])
@@ -31,4 +32,10 @@ export class ReactiveFormsComponent implements OnInit {
     (<FormArray>this.signUpForm.get('hobbies')).push(control);
   }
 
+  forbiddenNames(control :FormControl) : {[s:string] : boolean}{
+    if (this.forbiddenUsernames.indexOf(control.value) != -1){
+      return {'nameisForbidden' : true};
+    }
+    return null;
+  }
 }
