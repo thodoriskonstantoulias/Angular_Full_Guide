ee9f371d03291df07ad0dd3dd9ae332b7a788592
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-06 20:26:16 +0200

    #Setting up Firebase to our project (use your credentials to run)

--- Angular-Project/src/app/app.component.ts
+++ Angular-Project/src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular-Project';
 
+  ngOnInit(){
+    firebase.initializeApp({
+      apiKey: "Add yours here",
+      authDomain: "Add yours here"
+    });
+  }
 }
