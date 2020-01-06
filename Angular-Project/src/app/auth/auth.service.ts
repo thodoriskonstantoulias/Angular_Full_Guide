a63483c2b9306b203d0c8a6e1cc3b88f4ba0def8
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-06 22:52:39 +0200

    #Logout functionality, guard against creating new or edit recipe if user is not authenticated

--- Angular-Project/src/app/auth/auth.service.ts
+++ Angular-Project/src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
+import { Router } from '@angular/router';
 
 @Injectable({
     providedIn : 'root'
@@ -7,6 +8,8 @@ import * as firebase from 'firebase';
 
 export class AuthService{
     token : string;
+
+    constructor(private router:Router){}
 
     signupUser(email:string,password:string){
         firebase.auth().createUserWithEmailAndPassword(email,password)
@@ -19,6 +22,7 @@ export class AuthService{
         firebase.auth().signInWithEmailAndPassword(email,password)
                 .then(
                     response => {
+                        this.router.navigate(['/']);
                         firebase.auth().currentUser.getIdToken().then(
                             (token:string) => {
                                 this.token = token;
@@ -38,5 +42,14 @@ export class AuthService{
             }
         );
         return this.token;
+    }
+
+    isAuthenticated(){
+        return this.token != null;
+    }
+
+    logout(){
+        firebase.auth().signOut();
+        this.token = null;
     }
 }
--- Angular-Project/src/app/header/header.component.ts
+++ Angular-Project/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,7 @@ import { DataStorageService } from '../shared/data-storage.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private dataStorageService: DataStorageService) { }
+  constructor(private dataStorageService: DataStorageService,private authService:AuthService) { }
 
   ngOnInit() {
   }
@@ -21,6 +22,10 @@ export class HeaderComponent implements OnInit {
 
   onFetchData(){
     this.dataStorageService.getRecipes();
+  }
+
+  onLogout(){
+    this.authService.logout();
   }
   
 }
