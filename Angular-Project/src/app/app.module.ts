59684d710563931100e804acd950d6ab5944ddb9
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-07 22:13:48 +0200

    #Implementing lazy loading to Recipes page

--- Angular-Project/src/app/app.module.ts
+++ Angular-Project/src/app/app.module.ts
@@ -5,20 +5,20 @@ import { HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule, 
     HttpClientModule,
-    RecipesModule,
     AppRoutingModule,
     SharedModule,
     ShoppingListModule,
