eeef2e97a54db8c356889acc90fb945976cde12d
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-10 19:15:55 +0200

    Making our first animation

--- Angular-Animations/src/app/app.component.ts
+++ Angular-Animations/src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations : [
+    trigger('divState', [
+      state('normal', style({
+        'background-color' : 'red',
+        transform : 'translateX(0)'
+      })),
+      state('highlighted', style({
+        'background-color' : 'blue',
+        transform : 'translateX(100px)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800))
+    ])
+  ]
 })
 export class AppComponent {
+  state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
     onAdd(item) {
@@ -14,5 +30,9 @@ export class AppComponent {
 
     onDelete(item){
       this.list.splice(this.list.indexOf(item),1);
+    }
+
+    onAnimate(){
+      this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
     }
 }
