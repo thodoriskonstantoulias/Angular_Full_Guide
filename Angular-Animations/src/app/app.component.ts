a9453861190f98acf5de2f5ddc4d0e67529af186
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-10 21:11:22 +0200

    Animation callbacks

--- Angular-Animations/src/app/app.component.ts
+++ Angular-Animations/src/app/app.component.ts
@@ -121,4 +121,12 @@ export class AppComponent {
     onShrink(){
       this.wildState = 'shrunken';
     }
+
+    animationStarted(event){
+      console.log(event);
+    }
+
+    animationEnded(event){
+      console.log(event);
+    }
 }
