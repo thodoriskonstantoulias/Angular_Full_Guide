f8d4f1b21c61ed0df80487cbf070e44f8048d548
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-10 19:37:17 +0200

    Advanced animation with shrunking

--- Angular-Animations/src/app/app.component.ts
+++ Angular-Animations/src/app/app.component.ts
@@ -15,13 +15,31 @@ import { trigger, state, style, transition, animate} from '@angular/animations';
         'background-color' : 'blue',
         transform : 'translateX(100px)'
       })),
+      transition('normal <=> highlighted', animate(300)),
+      //transition('highlighted => normal', animate(800))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color' : 'red',
+        transform : 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color' : 'blue',
+        transform : 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color' : 'green',
+        transform : 'translateX(0px) scale(0.5)'
+      })),
       transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(800))
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *',animate(500))
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
     onAdd(item) {
@@ -34,5 +52,10 @@ export class AppComponent {
 
     onAnimate(){
       this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+      this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+    }
+
+    onShrink(){
+      this.wildState = 'shrunken';
     }
 }
