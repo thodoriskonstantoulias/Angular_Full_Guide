cad86a7af5c85c9ecd4f26a59d88b3632e360070
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-10 21:04:44 +0200

    Void state and Keyframes

--- Angular-Animations/src/app/app.component.ts
+++ Angular-Animations/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate} from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -42,6 +42,61 @@ import { trigger, state, style, transition, animate} from '@angular/animations';
         })),
         animate(500)
       ])
+    ]),
+    trigger('list1', [
+      state('in', style({
+        opacity : 1,
+        transform : 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity : 0,
+          transform : 'translateX(-100px)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void', [
+        animate(300, style({
+          transform : 'translateX(100px)',
+          opacity : 0
+        }))
+      ]),
+    ]),
+    trigger('list2', [
+      state('in', style({
+        opacity : 1,
+        transform : 'translateX(0)'
+      })),
+      transition('void => *', [
+        animate(1000,keyframes([
+          style({
+            transform : 'translateX(-100px)',
+            opacity : 0,
+            offset : 0
+          }),
+          style({
+            transform : 'translateX(-50px)',
+            opacity : 0.5,
+            offset : 0.3
+          }),
+          style({
+            transform : 'translateX(-20px)',
+            opacity : 1,
+            offset : 0.8
+          }),
+          style({
+            transform : 'translateX(0px)',
+            opacity : 1,
+            offset : 1
+          })
+        ]))
+      ]),
+      transition('* => void', [
+        animate(300, style({
+          transform : 'translateX(100px)',
+          opacity : 0
+        }))
+      ]),
     ])
   ]
 })
